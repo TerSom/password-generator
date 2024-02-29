7fcabd6b3a9db4f55e3ac65bfebb8f489d802c2b
TerSom <140949512+TerSom@users.noreply.github.com>
2024-02-29 19:15:19 +0700

    Add files via upload

--- sesi 8/scirpt.js
+++ sesi 8/scirpt.js
@@ -1,15 +1,15 @@
 let passwordLength = document.getElementById("passwordLength")
 let password = document.getElementById("password")
 
-function generatorPassword(){
+function generatorPassword(len){
  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
  const upperAlphabet = "abcdefghijklmnopqrstuvwxyz"
  const numeric = "0123456789"
- const sybol = "!@#$%^&*()_+-={}[]';:/?.,<>~"
+ const symbol = "!@#$%^&*()_+-={}[]';:/?.,<>~"
 
- const data = lowerAlphabet + upperAlphabet + numeric + sybol
- let generator = "";
- for (let index = 0; index < 10; index++ ) {
+ const data = lowerAlphabet + upperAlphabet + numeric + symbol
+ let generator = '';
+ for (let index = 0; index < len; index++ ) {
  generator += data [~~(Math.random() * data.length)];}
  return generator
 
